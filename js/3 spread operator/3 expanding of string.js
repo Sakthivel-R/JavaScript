53b77f7f6aa7fa47124f9b5c21963a8b2906d066
Sakthivel-R <79680761+Sakthivel-R@users.noreply.github.com>
2024-02-29 23:15:21 +0530

    Update 3 expanding of string.js

--- js/3 spread operator/3 expanding of string.js
+++ js/3 spread operator/3 expanding of string.js
@@ -4,5 +4,11 @@
 
 let greeting ="string to array"
 let charList =[...greeting]
+// [
+  's', 't', 'r', 'i',
+  'n', 'g', ' ', 't',
+  'o', ' ', 'a', 'r',
+  'r', 'a', 'y'
+]
 console.log(charList)
 console.log(charList[0])
